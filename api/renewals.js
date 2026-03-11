6502998dc0f3ff36b4fc2c794f8023a770fc9f77
olivoljan <olivkassen@gmail.com>
2026-03-11 11:31:11 +0100

    Fix Stripe starting_after pagination

--- api/renewals.js
+++ api/renewals.js
@@ -65,7 +65,7 @@ export default async function handler(req, res) {
       const page = await stripe.subscriptions.list({
         status: "active",
         limit: 100,
-        starting_after: startingAfter,
+        ...(startingAfter && { starting_after: startingAfter }),
         expand: [
           "data.customer",
           "data.items.data.price",
